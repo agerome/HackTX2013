486a3ccd55455edbb39e58c097fb717f76f6ec94
Alex Gerome <alex.gerome@utexas.edu>
2013-11-15 17:07:53 -0600

    PONG GAME!!!

---
+++ BounceBehavior.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BounceBehavior : MonoBehaviour {
+
+	public bool bounceX, bounceY = false;
+	public AudioClip bounceSound;
+
+	void OnCollisionEnter (Collision collision) {
+		if (!collision.gameObject.GetComponent<PuckBehavior> ()) {
+			return;
+		}
+
+		Vector3 vel = collision.rigidbody.velocity;
+		vel.x = bounceX ? -vel.x : vel.x;
+		vel.y = bounceY ? -vel.y : vel.y;
+		collision.rigidbody.velocity = 5*vel;
+
+		if (bounceSound) {
+			AudioSource.PlayClipAtPoint (bounceSound, Vector3.zero);
+		}
+	}
+}
---
+++ PuckBehavior.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PuckBehavior : MonoBehaviour {
+
+	private float speed = 100;
+
+	void Start () {
+		Reset ();
+	}
+
+	public void Reset () {
+		transform.position = Vector3.zero;
+		rigidbody.velocity = Vector3.one * speed;
+	}
+
+	void FixedUpdate () {
+		rigidbody.velocity = rigidbody.velocity.normalized * speed;
+	}
+}
